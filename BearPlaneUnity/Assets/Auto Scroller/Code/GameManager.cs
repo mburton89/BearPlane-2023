8028ae956d8124eb5adf9437eb5cb6a2353440c1
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2023-04-16 03:22:56 -0500

    all the things

--- BearPlaneUnity/Assets/Auto Scroller/Code/GameManager.cs
+++ BearPlaneUnity/Assets/Auto Scroller/Code/GameManager.cs
@@ -2,29 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    public Bear bear;
+    public MoveRight moveRight;
 
-    // Start is called before the first frame update
+    public int currentSpeed;
+
+    public TextMeshProUGUI currentSpeedText;
+    public TextMeshProUGUI bestSpeedText;
+
+    public float speedTextMultiplier;
+
+
     void Awake()
     {
         Instance = this;
         Application.targetFrameRate = 60;
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        bestSpeedText.SetText(PlayerPrefs.GetInt("BestSpeed").ToString());
+    }
+
     public void Restart()
     {
         StartCoroutine(RestartGame());
     }
 
+    private void Update()
+    {
+        UpdateUI();
+    }
 
     private IEnumerator RestartGame()
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }   
+    }
+
+    public void UpdateUI()
+    {
+        float currentSpeedFloat = moveRight.speed * speedTextMultiplier;
+        currentSpeed = (int)currentSpeedFloat;
+
+        currentSpeedText.SetText(currentSpeed.ToString());
+
+        if (currentSpeed > PlayerPrefs.GetInt("BestSpeed"))
+        {
+            PlayerPrefs.SetInt("BestSpeed", currentSpeed);
+            bestSpeedText.SetText(currentSpeedText.text);
+        }
+    }
 }
